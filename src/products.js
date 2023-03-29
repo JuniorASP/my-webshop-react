c9ae3147f80aae9045ee75c86a0c4b210a9c01b5
Junior <Junior-suarezperalta@hotmail.com>
2023-03-29 22:50:44 +0200

    .
    update

---
+++ src/products.js
@@ -0,0 +1,81 @@
+import product1 from './pictures/AppleTV.jpg'
+import product2 from './pictures/Airpods.jpg'
+import product3 from './pictures/AppleCharger.jpeg'
+import product4 from './pictures/AppleGlasses.jpg'
+import product5 from './pictures/AppleGlassesPink.jpeg'
+import product6 from './pictures/AppleMouse.jpeg'
+import product7 from './pictures/AppleWatch.jpeg'
+import product8 from './pictures/Macbook.jpeg'
+
+
+export const products = [
+{
+    id: 1,
+    productName: 'Apple TV',
+    price: 1300.0,
+    productImage: product1
+
+},
+{
+    id: 2,
+    productName: 'Airpods Pro',
+    price: 1300.0,
+    productImage: product2
+
+},
+{
+    id: 3,
+    productName: 'Apple SuperCharger+',
+    price: 1300.0,
+    productImage: product3
+
+},
+{
+    id: 4,
+    productName: 'Apple Glasses White',
+    price: 1300.0,
+    productImage: product4
+
+},
+{
+    id: 5,
+    productName: 'Apple Glasses Pink',
+    price: 1300.0,
+    productImage: product5
+
+},
+{
+    id: 6,
+    productName: 'Apple Mouse',
+    price: 1300.0,
+    productImage: product6
+
+},
+{
+    id: 7,
+    productName: 'Apple Watch',
+    price: 1300.0,
+    productImage: product7
+
+},
+{
+    id: 8,
+    productName: 'MacBook Air2',
+    price: 1300.0,
+    productImage: product8
+
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
